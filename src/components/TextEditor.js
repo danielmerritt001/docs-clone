b8602cb026dc87b7f57e447f9bf3f3695b097916
Daniel Merritt <daniel.mafou@gmail.com>
2023-09-05 09:56:20 -0400

    basic text editor implementation

--- src/components/TextEditor.js
+++ src/components/TextEditor.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
+import Quill from 'quill';
+import "quill/dist/quill.snow.css"
 
 function TextEditor() {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return
+    wrapper.innerHTML = ''
+    const editor = document.createElement('div')
+    wrapper.append(editor)
+    new Quill(editor, { theme: "snow"})
+  }, [])
   return (
-    <div>Text Editor</div>
+    <div id='container' ref={wrapperRef}></div>
   );
 }
 
