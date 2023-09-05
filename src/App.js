c2c20bf3e87aadf13f612935295614a400fd61c1
Daniel Merritt <daniel.mafou@gmail.com>
2023-09-05 15:10:14 -0400

    create unique url for doc

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Fragment } from "react";
 import TextEditor from "./components/TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom'
+import { v4 as uuidV4} from 'uuid'
 
 function App() {
   return (
-    <TextEditor />
+    <Router>
+      <Fragment>
+        <Routes>
+          <Route path="/" exact element={<Navigate to={`/documents/${uuidV4()}`} />} />
+          <Route path="documents/:id" exact element={<TextEditor />} />
+
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
