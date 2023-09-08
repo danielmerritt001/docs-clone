65d06bfa0276065568ee956384a0402c1043b06b
Daniel Merritt <daniel.mafou@gmail.com>
2023-09-08 10:31:50 -0400

    separate doc rooms

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from 'react'
 import Quill from 'quill';
 import "quill/dist/quill.snow.css"
 import { io } from "socket.io-client"
+import { useParams } from 'react-router-dom';
 
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false]}],
@@ -16,8 +17,23 @@ const TOOLBAR_OPTIONS = [
 ]
 
 function TextEditor() {
+  const { id: documentId } = useParams()
   const [socket, setSocket] = useState()
   const [quill, setQuill] = useState()
+
+  useEffect(() =>{
+
+  }, [])
+
+  useEffect(() =>{
+    if (socket == null || quill == null) return
+    socket.once("load-document", document => {
+      quill.setContents(document)
+      quill.enable()
+    })
+    socket.emit('get-document', documentId)
+  }, [socket, quill, documentId])
+
   useEffect(() => {
     const s = io("http://localhost:3001")
     setSocket(s)
@@ -55,6 +71,8 @@ function TextEditor() {
     const editor = document.createElement('div')
     wrapper.append(editor)
     const q = new Quill(editor, { theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS}})
+    q.disable()
+    q.setText('Loading...')
     setQuill(q)
   }, [])
   return (
